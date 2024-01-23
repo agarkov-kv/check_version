49b608a110b04263075b490455141e6cd347fdbb
agarkov-kv <Unisolll48@yandex.ru>
2024-01-23 23:02:08 +0300

    Version 1.1

--- src/test/java/src/test/java/LoginTests.java
+++ src/test/java/src/test/java/LoginTests.java
@@ -3,19 +3,22 @@ package src.test.java;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selenide.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class LoginTests {
     @Test
     void successlogintest() {
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("intl.accept_languages", "de-DE,de");
+        options.setExperimentalOption("prefs", prefs);
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
         Configuration.holdBrowserOpen = true;
-        open("https://qa.guru/cms/system/login");
-        $("[name=email]").setValue("pekite4449@chambile.com");
-        $("[name=password]").setValue("$&C14D3dd1");
-        $(".btn-success").click();
-        $(".has-arrow").click();
-        $(".logined-form").shouldHave(Condition.text("Account"));
+        open("https://appgallery.huawei.ru/#/app/C100359849");
+        $x("/html/body/div/div[3]/div/div[3]/div[7]/div/div[2]/div[1]/div[3]/div[2]").shouldHave(Condition.text("58.24.0"));
     }
 }
-
-
